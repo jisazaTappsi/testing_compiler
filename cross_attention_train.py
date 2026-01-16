a4ea8cbeb54daea6a5c16b6b7b763a9445ff2d7a
Juanito <juan@peaku.co>
2026-01-16 21:11:04 +0100

    Cleaning up some cross attention code

--- cross_attention_train.py
+++ cross_attention_train.py
@@ -27,23 +27,13 @@ dropout = 0.2
 n_head = 6  # 4
 n_embed = 64 * n_head  # 32
 train_split_ratio = 0.8
-model_type = 'cross-attention'#'self-attention'
-max_pairs = 1_000_000
+max_pairs = 10_000
 device = get_device()
 
 torch.manual_seed(1337)
 
 
-def get_self_attention_batch(my_data):
-    """my_data is a single dimension very long tensor"""
-    random_sample_indexes = torch.randint(len(my_data) - block_size, (batch_size,))
-    x_out = torch.stack([my_data[i:i+block_size] for i in random_sample_indexes])
-    y = torch.stack([my_data[i+1:i+block_size+1] for i in random_sample_indexes])
-    x_out, y = x_out.to(device), y.to(device)
-    return {'x_out': x_out, 'y': y}
-
-
-def get_cross_attention_batch(my_data):
+def get_batch(my_data):
     """my_data is a two dimension stacked pair tensor"""
     random_sample_indexes = torch.randint(len(my_data), (batch_size,))
     x_out = torch.stack([torch.tensor(my_data[i]['x_out'][:block_size]) for i in random_sample_indexes])
@@ -51,15 +41,6 @@ def get_cross_attention_batch(my_data):
     y = torch.stack([torch.tensor(my_data[i]['x_out'][1:block_size+1]) for i in random_sample_indexes])
     x_out, x_in, y = x_out.to(device), x_in.to(device), y.to(device)
     return {'x_out': x_out, 'x_in': x_in, 'y': y}
-
-
-def get_batch(my_data):
-    if model_type == 'self-attention':
-        return get_self_attention_batch(my_data)
-    elif model_type == 'cross-attention':
-        return get_cross_attention_batch(my_data)
-    else:
-        raise ValueError('Unknown type')
 
 
 @torch.no_grad()
@@ -78,8 +59,6 @@ def estimate_loss(my_data, model):
 
 
 class HeadCross(nn.Module):
-    """one head self attention"""
-
     def __init__(self, head_size):
         super().__init__()
         self.key = nn.Linear(n_embed, head_size, bias=False)
@@ -106,8 +85,6 @@ class HeadCross(nn.Module):
 
 
 class MaskedHead(nn.Module):
-    """one head self attention"""
-
     def __init__(self, head_size):
         super().__init__()
         self.key = nn.Linear(n_embed, head_size, bias=False)
@@ -186,16 +163,14 @@ class BlockCross(nn.Module):
         self.ln2_cross = nn.LayerNorm(n_embed)
         self.ffwd_cross = FeedForward(n_embed)
 
-    def forward(self, x_out, x_in=None):
-        if x_in is not None:
-            # Self-attention on x_in (pass only one arg, so x_in=None in HeadCross)
-            x_in = x_in + self.multi_cross(self.ln1_cross(x_in))
-            x_in = x_in + self.ffwd_cross(self.ln2_cross(x_in))
-        else:  # self attention
-            x_in = x_out
+    def forward(self, x_out, x_in):
+        # Small left branch in "Attention is all you need" paper
+        x_in = x_in + self.multi_cross(self.ln1_cross(x_in))  # Operates on self-attention mode
+        x_in = x_in + self.ffwd_cross(self.ln2_cross(x_in))
 
-        x_out = x_out + self.masked_multi(self.ln1(x_out))
-        x_out = x_out + self.multi(self.ln2(x_out), x_in)
+        # Big main branch in "Attention is all you need" paper
+        x_out = x_out + self.masked_multi(self.ln1(x_out))  # Operates on self-attention mode
+        x_out = x_out + self.multi(self.ln2(x_out), x_in)  # Operates on cross-attention mode
         x_out = x_out + self.ffwd(self.ln3(x_out))
         return x_out, x_in
 
@@ -226,10 +201,7 @@ class CrossAttentionTransformer(nn.Module):
         pos_emb = position_table(torch.arange(T, device=device))
         return tok_emb + pos_emb
 
-    def forward(self, x_out, x_in=None, y=None):
-        if x_in is None:
-            x_in = x_out
-
+    def forward(self, x_out, x_in, y=None):
         emb = CrossAttentionTransformer.get_embedding(x_out, token_table=self.emb_table,
                                                       position_table=self.position_emb_table)
         emb_cross = CrossAttentionTransformer.get_embedding(x_in, token_table=self.emb_table_cross,
@@ -310,7 +282,7 @@ def get_sample_val_data(num=20):
     return pairs
 
 
-def get_cross_attention_data():
+def get_data():
     """We translate from EN to ES, ie our x_in=EN, while x_out=ES"""
     en_sentences = get_first_rows_fast('OpenSubtitles.en', max_pairs)
     es_sentences = get_first_rows_fast('OpenSubtitles.es', max_pairs)
@@ -331,16 +303,6 @@ def get_cross_attention_data():
         en_encoded = [tokens['start_in']] + en_encoded + [tokens['end_in']] * max(1, block_size - len(en_encoded))
         es_encoded = es_encoded[:cut_size]
         es_encoded = [tokens['start_out']] + es_encoded + [tokens['end_out']] * max(1, block_size - len(es_encoded))
-        #print(en_encoded)
-        #print(es_encoded)
-        """
-        assert len(en_encoded) == 65
-        assert len(es_encoded) == 65
-        assert en_encoded[0] == tokens['start_in']
-        assert en_encoded[-1] == tokens['end_in']
-        assert es_encoded[0] == tokens['start_out']
-        assert es_encoded[-1] == tokens['end_out']
-        """
         translation_pairs.append({'x_in': en_encoded, 'x_out': es_encoded})
 
     train_pairs = translation_pairs[:n]
@@ -352,42 +314,14 @@ def get_cross_attention_data():
     }, out_merges, in_merges
 
 
-def get_self_attention_data():
-    with open('shakespear.txt', 'r') as f:
-        text = f.read()
-
-    tokens = list([int(i) for i in text.encode('utf-8')])
-    print(f'length tokens: {len(tokens)}')
-    print(f'length text: {len(text)}')
-
-    merges = bpe.train_merges(tokens, bpe.out_vocab_size)
-
-    # Justa a long 1 d tensor. bah!
-    encoded_data = torch.tensor(bpe.encode(text, merges), dtype=torch.long)
-    n = int(train_split_ratio * len(encoded_data))
-    return {
-        'train': encoded_data[:n],
-        'val': encoded_data[n:]
-    }, merges, {}
-
-
-def get_data_and_merges():
-    if model_type == 'self-attention':
-        return get_self_attention_data()
-    elif model_type == 'cross-attention':
-        return get_cross_attention_data()
-    else:
-        raise Exception(f'Unknown type {model_type}')
-
-
 def train():
-    data, out_merges, in_merges = get_data_and_merges()
+    data, out_merges, in_merges = get_data()
 
     model = CrossAttentionTransformer()
     model = model.to(device)
     try:
         model.load_state_dict(torch.load('cross_attention_model.pth'))
-        print('training old model')
+        print('training an existing model')
         model.train()
     except FileNotFoundError:
         print('Creating model from scratch')
