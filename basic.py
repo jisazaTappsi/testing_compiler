5ae2599384f3571a321fcda8ea8acba098fd0657
Juanito <juan@peaku.co>
2026-03-29 00:10:43 +0100

    fixes stupid bug that made half of data generated fail

--- basic.py
+++ basic.py
@@ -222,7 +222,7 @@ class Lexer:
             self.advance()
             tok_type = ARROW
 
-        return Token(tok_type, pos_start, pos_start=pos_start, pos_end=self.pos)
+        return Token(tok_type, pos_start=pos_start, pos_end=self.pos)
 
     def make_not_equals(self):
         pos_start = self.pos.copy()
